81fcaab241b631cb49cd0cf5e3b0345a4330a734
Maciej Borzecki <maciej.borzecki@open-rnd.pl>
2015-03-26 13:23:11 +0100

    setup: setuptools support
    Change-Id: I547018e83bd2f57d1256a50c156c95e9927e6965

---
+++ setup.py
@@ -0,0 +1,33 @@
+#
+# Copyright (c) 2015, Open-RnD Sp. z o.o.  All rights reserved.
+#
+from setuptools import setup, find_packages
+import os
+
+NAME='roskr'
+VERSION = '0.1'
+
+install_requires = ['sparts']
+tests_require = []
+
+ROOT = os.path.dirname(__file__)
+
+def read(fpath):
+    """Load file contents"""
+    with open(os.path.join(ROOT, fpath)) as inf:
+        return inf.read()
+
+
+setup(
+    name=NAME,
+    version=VERSION,
+    packages=find_packages(exclude=['tests', 'tests.*']),
+    description="Ros3D KR",
+    long_description=read("README.rst"),
+
+    install_requires=install_requires,
+    tests_require=tests_require,
+    author='OpenRnD',
+    author_email='ros3d@open-rnd.pl',
+    license='closed',
+)
