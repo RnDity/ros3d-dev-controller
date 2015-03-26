7db6f6aa9a2c0136814c814538dba5c10a81f0a6
Maciej Borzecki <maciej.borzecki@open-rnd.pl>
2015-03-26 13:20:35 +0100

    cmd: add helper for starting from command line
    Change-Id: I225da2199bc71fcdbb367b9f2c210dc3f64e1791

--- ros3dkr.py
+++ ros3dkr.py
@@ -3,14 +3,6 @@
 # Copyright (c) 2015, Open-RnD Sp. z o.o.  All rights reserved.
 #
 
-import logging
-import argparse
-from ros3dkr.service import Ros3DKRService
-
-
-def main():
-    Ros3DKRService.initFromCLI()
-
-
 if __name__ == '__main__':
+    from ros3dkr.cmd import main
     main()
---
+++ ros3dkr/cmd.py
@@ -0,0 +1,10 @@
+#
+# Copyright (c) 2015, Open-RnD Sp. z o.o.  All rights reserved.
+#
+
+from ros3dkr.service import Ros3DKRService
+
+
+def main():
+    Ros3DKRService.initFromCLI()
+
