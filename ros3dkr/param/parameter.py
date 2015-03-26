8f8617b915e70665909d1a0f209ce8c95bbb0e33
Maciej Borzecki <maciej.borzecki@open-rnd.pl>
2015-03-26 16:03:59 +0100

    params: move parameters to separate module
    Change-Id: I9d32a8a11e4a5b4d5aa60efb432b7b121a01dbe5

---
+++ ros3dkr/param/parameter.py
@@ -0,0 +1,54 @@
+#
+# Copyright (c) 2015, Open-RnD Sp. z o.o.  All rights reserved.
+#
+"""System parameters wrappers"""
+
+from __future__ import absolute_import
+
+import logging
+
+_log = logging.getLogger(__name__)
+
+
+class ParameterStatus(object):
+    """Paramter status wrapper"""
+    STATUS_TYPE_HARDWARE = 'hardware'
+    STATUS_TYPE_SOFTWARE = 'software'
+
+    def __init__(self, read=True, write=True,
+                 status_type=STATUS_TYPE_HARDWARE):
+        self.read = read
+        self.write = write
+        self.status = status_type
+
+    def as_dict(self):
+        """Return a JSON serializable dict"""
+        return {
+            'read': bool(self.read),
+            'write': bool(self.write),
+            'status': str(self.status)
+        }
+
+
+class Parameter(object):
+    """System parameter wrapper"""
+
+    def __init__(self, name, value, value_type,
+                 status, min_val, max_val):
+        self.name = name
+        self.value = value
+        self.value_type = value_type
+        self.status = status
+        self.min_value = min_val
+        self.max_value = max_val
+
+    def as_dict(self):
+        """Return a JSON serializable dict"""
+        return {
+            "value": self.value,
+            "valueType": self.value_type.__name__,
+            "status:": self.status.as_dict(),
+            "minValue": self.min_value,
+            "maxValue": self.max_value
+        }
+
--- ros3dkr/param/store.py
+++ ros3dkr/param/store.py
@@ -14,49 +14,10 @@
 from __future__ import absolute_import
 
 import logging
+import sys
+from ros3dkr.param.parameter import Parameter, ParameterStatus
 
 _log = logging.getLogger(__name__)
-
-
-class ParameterStatus(object):
-    """Paramter status wrapper"""
-    STATUS_TYPE_HARDWARE = 'hardware'
-    STATUS_TYPE_SOFTWARE = 'software'
-
-    def __init__(self, read=True, write=True,
-                 status_type=STATUS_TYPE_HARDWARE):
-        self.read = read
-        self.write = write
-        self.status = status_type
-
-    def as_dict(self):
-        return {
-            'read': bool(self.read),
-            'write': bool(self.write),
-            'status': str(self.status)
-        }
-
-
-class Parameter(object):
-    """System parameter wrapper"""
-
-    def __init__(self, name, value, value_type,
-                 status, min_val, max_val):
-        self.name = name
-        self.value = value
-        self.value_type = value_type
-        self.status = status
-        self.min_value = min_val
-        self.max_value = max_val
-
-    def as_dict(self):
-        return {
-            "value": self.value,
-            "valueType": self.value_type.__name__,
-            "status:": self.status.as_dict(),
-            "minValue": self.min_value,
-            "maxValue": self.max_value
-        }
 
 
 class ParametersStore(object):
