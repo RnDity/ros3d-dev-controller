fb5c7b35d7c3e4fd96dbf50188af96a51c706b09
Maciej Borzecki <maciej.borzecki@open-rnd.pl>
2015-03-27 15:50:14 +0100

    service: add servo tasks
    Change-Id: I422985c5a10bc3e6dc12c8f7adbffbf5f187355a

--- ros3dkr/service.py
+++ ros3dkr/service.py
@@ -4,12 +4,13 @@
 from sparts.vservice import VService
 from ros3dkr.web import WebAPITask
 from ros3dkr.bus import Ros3DDBusTask
-
+from ros3dkr.bus.servo import ServoTask
 
 class Ros3DKRService(VService):
     """Ros3D KR services wrapper"""
 
     TASKS = [
         WebAPITask,
-        Ros3DDBusTask
+        Ros3DDBusTask,
+        ServoTask
     ]
