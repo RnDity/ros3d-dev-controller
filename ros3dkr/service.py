bc9d144d2aade11f51cdfd42f6cd32b8e39fa127
Maciej Borzecki <maciej.borzecki@open-rnd.pl>
2015-04-02 15:25:06 +0200

    service: load configuration from file
    User defined config can be passed as --config-file parameter.
    
    Change-Id: Ia5e8ab96e3009a51f808a363bc968b1ebc7807f8

--- ros3dkr/service.py
+++ ros3dkr/service.py
@@ -2,9 +2,11 @@
 #
 
 from sparts.vservice import VService
+from sparts.sparts import option
 from ros3dkr.web import WebAPITask
 from ros3dkr.bus import Ros3DDBusTask
 from ros3dkr.bus.servo import ServoTask
+from ros3dkr.util import ConfigLoader
 
 class Ros3DKRService(VService):
     """Ros3D KR services wrapper"""
@@ -13,3 +15,13 @@ class Ros3DKRService(VService):
         WebAPITask,
         ServoTask
     ]
+
+    config_file = option(default=ConfigLoader.DEFAULT_PATH,
+                         help='Ros3D configuration file')
+
+    def __init__(self, *args, **kwargs):
+        super(Ros3DKRService, self).__init__(*args, **kwargs)
+
+        self.logger.debug('loading configuration from %s',
+                          self.options.config_file)
+        self.config = ConfigLoader(self.options.config_file)
