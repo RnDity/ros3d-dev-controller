857fefa4be6677f22a6cc30f34b0683833a4e28f
Maciej Borzecki <maciej.borzecki@open-rnd.pl>
2015-04-28 12:06:16 +0200

    test/parameter: tests for Parameter()
    Change-Id: Id9065b48588749a04f1f97b7208acf1cada53407

---
+++ tests/tests_parameter.py
@@ -0,0 +1,45 @@
+#
+# Copyright (c) 2015, Open-RnD Sp. z o.o.  All rights reserved.
+#
+"""System parameters wrappers"""
+
+from __future__ import absolute_import
+
+import unittest
+import mock
+
+from ros3dkr.param.parameter import Parameter, ParameterStatus
+
+class ParameterTestCase(unittest.TestCase):
+    def test_new_no_status(self):
+        par = Parameter('foo', 'bar', str)
+
+        self.assertEqual(par.name, 'foo')
+        self.assertEqual(par.value, 'bar')
+        self.assertIsInstance(par.value, str)
+        self.assertEqual(par.value_type, str)
+
+        self.assertTrue(hasattr(par, 'status'))
+        status = par.status
+        self.assertIsInstance(status, ParameterStatus)
+        self.assertEqual(status.read, True)
+        self.assertEqual(status.write, True)
+        self.assertEqual(status.status, ParameterStatus.HARDWARE)
+
+    def test_new_status(self):
+        st = ParameterStatus()
+
+        par = Parameter('foo', 1, int, status=st, min_val=0, max_val=10)
+
+        self.assertEqual(par.name, 'foo')
+        self.assertEqual(par.value, 1)
+        self.assertIsInstance(par.value, int)
+        self.assertEqual(par.value_type, int)
+        self.assertEqual(par.min_value, 0)
+        self.assertEqual(par.max_value, 10)
+        self.assertIs(par.status, st)
+
+    def test_new_bad_status(self):
+        self.assertRaises(AssertionError, Parameter,
+                          'foo', 'bar', str, status=1)
+
