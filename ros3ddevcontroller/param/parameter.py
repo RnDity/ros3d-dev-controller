bf86e26f99e7dab2396080931af18e12e46bc11b
Maciej Borzecki <maciej.borzecki@open-rnd.pl>
2015-11-27 14:58:52 +0100

    param: add parameter evaluator class
    When a parameter is evaluated (and not directly read from hardware), the
    user is expected to implement and pass an evaluator class
    
    Change-Id: Id0ff82c0488839c3e98c594836c334d45f4fbb71

--- ros3ddevcontroller/param/parameter.py
+++ ros3ddevcontroller/param/parameter.py
@@ -26,7 +26,8 @@ class Parameter(object):
     """System parameter wrapper"""
 
     def __init__(self, name, value, value_type,
-                 status=None, min_val=None, max_val=None):
+                 status=None, min_val=None, max_val=None,
+                 evaluated=False, evaluator=None):
         """Initialize a parameter
 
         :param str name: parameter name
@@ -35,6 +36,8 @@ class Parameter(object):
         :param ParameterStatus status: status info, if None, defaults to a hardware paramter
         :param min_val: minimum value
         :param max_val: maximum value
+        :param evaluated: True if value is evaluated
+        :param evaluator: Class that will be called to evaluate the parameter
         """
         self.name = name
         self.value = value
@@ -49,3 +52,13 @@ class Parameter(object):
 
         self.min_value = min_val
         self.max_value = max_val
+
+        self.evaluated = evaluated
+        self.evaluator = evaluator
+
+
+class Evaluator(object):
+    NAME = 'unset'
+
+    def __call__(self):
+        raise NotImplementedError('Evaluation for {} not implemented'.format(self.__name__))
