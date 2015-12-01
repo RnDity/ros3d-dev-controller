b84cae0f20a7a83df93de3c26972b06e14d4e8e6
Maciej Borzecki <maciej.borzecki@open-rnd.pl>
2015-12-01 12:14:44 +0100

    controller: add core logic class
    Add a Controller class that implements core parameter handling logic.
    
    Change-Id: I1965ba354e15d38bc1080dfc96ce389d3a4bbc18

---
+++ ros3ddevcontroller/controller.py
@@ -0,0 +1,33 @@
+"""Core Ros3D controller logic"""
+import logging
+from ros3ddevcontroller.param import ParametersStore
+
+
+class Controller(object):
+    def __init__(self):
+        self.logger = logging.getLogger(__name__)
+        self.servo = None
+        self.snapshots_location = None
+
+    def set_servo(self, servo):
+        """Servo interface"""
+        self.servo = servo
+
+    def set_snapshots_location(self, loc):
+        """Set location of snapshots"""
+        self.snapshots_location = loc
+
+    def apply_param(self, param):
+        """Apply parameter"""
+        raise NotImplementedError('not implemented')
+
+    def get_params(self):
+        """Return a dict with all parameters in the system"""
+        return ParametersStore.parameters_as_dict()
+
+    def take_snapshot(self):
+        raise NotImplementedError('not implemented')
+
+    def list_snapshots(self):
+        raise NotImplementedError('not implemented')
+
