59a20992746bcbb29e7c877544d786d4a48ff97e
Michal Wlodarczyk <michal.wlodarczyk.mail@gmail.com>
2015-12-10 14:57:16 +0100

    tests: evaluation for focal length changed added
    Change-Id: Ifc8829292e20028eab95b9f5c60501c8953a71da

--- tests/test_evaluator.py
+++ tests/test_evaluator.py
@@ -39,3 +39,26 @@ class EvaluationTestCase(unittest.TestCase):
 
         convergence_deg = ParametersStore.get_value('convergence_deg')
         self.assertEquals(round(convergence_deg, 2), 0.22)
+
+    def test_focal_length_changed(self):
+
+        #Set initial values
+        ParametersStore.set('focal_length_mm', 25, evaluate=False)
+        ParametersStore.set('baseline_mm', 50.0, evaluate=False)
+        ParametersStore.set('frame_width_mm', 15.84, evaluate=False)
+        ParametersStore.set('frame_height_mm', 8.91, evaluate=False )
+        ParametersStore.set('distance_screen_m', 3.15, evaluate=False)
+        ParametersStore.set('distance_near_m', 1.04, evaluate=False)
+
+        #Change focal_length to check evaluation
+        ParametersStore.set('focal_length_mm', 20, evaluate=True)
+
+        #Check new values
+        parallax_near_percent = ParametersStore.get_value('parallax_near_percent')
+        self.assertEquals(round(parallax_near_percent, 2), -4.07)
+
+        real_width_near_m = ParametersStore.get_value('real_width_near_m')
+        self.assertEquals(round(real_width_near_m, 2), 0.82)
+
+        real_height_near_m = ParametersStore.get_value('real_height_near_m')
+        self.assertEquals(round(real_height_near_m, 2), 0.46)
