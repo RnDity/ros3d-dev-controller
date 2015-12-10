60f503006bd5fe25414a05d10e74fa22afa5e11b
Michal Wlodarczyk <michal.wlodarczyk.mail@gmail.com>
2015-12-10 14:02:55 +0100

    tests: evaluation for convergence changed added
    Change-Id: If65220bd2157e91e7146272ca457f4b26edc5d89

--- tests/test_evaluator.py
+++ tests/test_evaluator.py
@@ -18,3 +18,24 @@ class EvaluationTestCase(unittest.TestCase):
 
     def tearDown(self):
         ParametersStore.clear_parameters()
+
+    def test_baseline_changed(self):
+
+        #Set initial values
+        ParametersStore.set('frame_width_px', 1920, evaluate=False)
+        ParametersStore.set('focal_length_mm', 25, evaluate=False)
+        ParametersStore.set('baseline_mm', 50.0, evaluate=False)
+        ParametersStore.set('frame_width_mm', 15.84, evaluate=False)
+        ParametersStore.set('distance_screen_m', 5.2, evaluate=False)
+        ParametersStore.set('distance_object1_m', 4.4, evaluate=False)
+        ParametersStore.set('distance_object2_m', 9.5, evaluate=False)
+
+        #Change baseline to different value so convergence evaluation starts
+        ParametersStore.set('baseline_mm', 20, evaluate=True)
+
+        #Check new values
+        convergence_px = ParametersStore.get_value('convergence_px')
+        self.assertEquals(round(convergence_px, 2), 12.03)
+
+        convergence_deg = ParametersStore.get_value('convergence_deg')
+        self.assertEquals(round(convergence_deg, 2), 0.22)
